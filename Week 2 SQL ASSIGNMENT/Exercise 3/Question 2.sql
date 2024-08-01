d6d33e9ca1e86260cb6601e4bb7b12ef47035dd9
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-08-02 02:28:03 +0530

    Update Question 2.sql

--- Week 2 SQL ASSIGNMENT/Exercise 3/Question 2.sql
+++ Week 2 SQL ASSIGNMENT/Exercise 3/Question 2.sql
@@ -1,22 +1,26 @@
+Scenario 2: The bank wants to implement a bonus scheme for employees based on their performance.
+o	Question: Write a stored procedure UpdateEmployeeBonus that updates the salary of employees in a given department by adding a bonus percentage passed as a parameter.
+
+
 CREATE OR REPLACE PROCEDURE UpdateEmployeeBonus (
     p_Department IN Employees.Department%TYPE,
     p_BonusPercentage IN NUMBER
 ) IS
 BEGIN
-    -- Update the salary for employees in the specified department by applying the bonus percentage
+    
     UPDATE Employees
     SET Salary = Salary * (1 + p_BonusPercentage / 100)
     WHERE Department = p_Department;
     
-    -- Commit the transaction to save changes
+   
     COMMIT;
     
     DBMS_OUTPUT.PUT_LINE('Salaries updated with a bonus of ' || p_BonusPercentage || '% for department ' || p_Department || '.');
 EXCEPTION
     WHEN OTHERS THEN
-        -- Handle any exceptions that occur
+       
         DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
-        ROLLBACK; -- Rollback the transaction in case of an error
+        ROLLBACK; 
 END UpdateEmployeeBonus;
 /
 
