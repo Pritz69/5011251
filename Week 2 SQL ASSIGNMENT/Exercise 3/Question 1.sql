c2942127b6ba20ab6d9bb0bb3f972cb196061d3d
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-08-02 02:26:55 +0530

    Update Question 1.sql

--- Week 2 SQL ASSIGNMENT/Exercise 3/Question 1.sql
+++ Week 2 SQL ASSIGNMENT/Exercise 3/Question 1.sql
@@ -1,21 +1,25 @@
+Scenario 1: The bank needs to process monthly interest for all savings accounts.
+o	Question: Write a stored procedure ProcessMonthlyInterest that calculates and updates the balance of all savings accounts by applying an interest rate of 1% to the current balance.
+
+
 CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest (
     p_InterestRate IN NUMBER
 ) IS
 BEGIN
-    -- Update the balance for all savings accounts by applying the specified interest rate
+    
     UPDATE Accounts
     SET Balance = Balance * (1 + p_InterestRate / 100)
     WHERE AccountType = 'Savings';
     
-    -- Commit the transaction to save changes
+    
     COMMIT;
     
     DBMS_OUTPUT.PUT_LINE('Monthly interest applied to all savings accounts with an interest rate of ' || p_InterestRate || '%.');
 EXCEPTION
     WHEN OTHERS THEN
-        -- Handle any exceptions that occur
+       
         DBMS_OUTPUT.PUT_LINE('Error: ' || SQLERRM);
-        ROLLBACK; -- Rollback the transaction in case of an error
+        ROLLBACK;
 END ProcessMonthlyInterest;
 /
 BEGIN
