f74e82bf519e6ad1ede422e0b22a36e3b099bf2d
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-08-02 02:34:24 +0530

    Update Question 1.sql

--- Week 2 SQL ASSIGNMENT/Exercise 5/Question 1.sql
+++ Week 2 SQL ASSIGNMENT/Exercise 5/Question 1.sql
@@ -6,7 +6,7 @@ CREATE OR REPLACE TRIGGER UpdateCustomerLastModified
 BEFORE UPDATE ON Customers
 FOR EACH ROW
 BEGIN
-    -- Update the LastModified column to the current date
+    
     :NEW.LastModified := SYSDATE;
 END;
 /
