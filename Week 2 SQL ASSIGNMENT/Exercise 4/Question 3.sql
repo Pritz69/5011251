b7009e12dbc7e84fbf61219c3f71ffe84575c9cf
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-08-02 18:58:58 +0530

    Update Question 3.sql

--- Week 2 SQL ASSIGNMENT/Exercise 4/Question 3.sql
+++ Week 2 SQL ASSIGNMENT/Exercise 4/Question 3.sql
@@ -1,5 +1,5 @@
-Scenario 3: Check if a customer has sufficient balance before making a transaction.
-o	Question: Write a function HasSufficientBalance that takes an account ID and an amount as input and returns a boolean indicating whether the account has at least the specified amount.
+---Scenario 3: Check if a customer has sufficient balance before making a transaction.
+---o	Question: Write a function HasSufficientBalance that takes an account ID and an amount as input and returns a boolean indicating whether the account has at least the specified amount.
 
 
 CREATE OR REPLACE FUNCTION HasSufficientBalance (
