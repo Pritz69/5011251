dde79633b73434f412fe0fa11215a8cb814d3162
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-08-02 18:58:47 +0530

    Update Question 2.sql

--- Week 2 SQL ASSIGNMENT/Exercise 4/Question 2.sql
+++ Week 2 SQL ASSIGNMENT/Exercise 4/Question 2.sql
@@ -1,5 +1,5 @@
-Scenario 2: The bank needs to compute the monthly installment for a loan.
-o	Question: Write a function CalculateMonthlyInstallment that takes the loan amount, interest rate, and loan duration in years as input and returns the monthly installment amount.
+---Scenario 2: The bank needs to compute the monthly installment for a loan.
+---o	Question: Write a function CalculateMonthlyInstallment that takes the loan amount, interest rate, and loan duration in years as input and returns the monthly installment amount.
 
 CREATE OR REPLACE FUNCTION CalculateMonthlyInstallment (
     p_LoanAmount IN NUMBER,
