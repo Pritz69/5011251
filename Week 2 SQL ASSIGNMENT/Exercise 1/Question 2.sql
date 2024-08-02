a9ee1a4877e0b13009a70a73dd4941a2f789b6f6
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-08-02 18:56:37 +0530

    Update Question 2.sql

--- Week 2 SQL ASSIGNMENT/Exercise 1/Question 2.sql
+++ Week 2 SQL ASSIGNMENT/Exercise 1/Question 2.sql
@@ -1,5 +1,5 @@
-Scenario 2: A customer can be promoted to VIP status based on their balance.
-o	Question: Write a PL/SQL block that iterates through all customers and sets a flag IsVIP to TRUE for those with a balance over $10,000.
+---Scenario 2: A customer can be promoted to VIP status based on their balance.
+---o	Question: Write a PL/SQL block that iterates through all customers and sets a flag IsVIP to TRUE for those with a balance over $10,000.
 
 
 ALTER TABLE Customers
