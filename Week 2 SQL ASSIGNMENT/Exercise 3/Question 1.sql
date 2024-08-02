7aa8fc5aea466ca35f4bd5d486e0697a55c3874b
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-08-02 18:57:52 +0530

    Update Question 1.sql

--- Week 2 SQL ASSIGNMENT/Exercise 3/Question 1.sql
+++ Week 2 SQL ASSIGNMENT/Exercise 3/Question 1.sql
@@ -1,5 +1,5 @@
-Scenario 1: The bank needs to process monthly interest for all savings accounts.
-o	Question: Write a stored procedure ProcessMonthlyInterest that calculates and updates the balance of all savings accounts by applying an interest rate of 1% to the current balance.
+---Scenario 1: The bank needs to process monthly interest for all savings accounts.
+---o	Question: Write a stored procedure ProcessMonthlyInterest that calculates and updates the balance of all savings accounts by applying an interest rate of 1% to the current balance.
 
 
 CREATE OR REPLACE PROCEDURE ProcessMonthlyInterest (
