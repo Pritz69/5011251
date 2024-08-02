db18879a4d0baa8ad214d24a4dd2f82f2c6f7ad5
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-08-02 18:57:20 +0530

    Update Question 2.sql

--- Week 2 SQL ASSIGNMENT/Exercise 2/Question 2.sql
+++ Week 2 SQL ASSIGNMENT/Exercise 2/Question 2.sql
@@ -1,5 +1,5 @@
-Scenario 2: Manage errors when updating employee salaries.
-o	Question: Write a stored procedure UpdateSalary that increases the salary of an employee by a given percentage. If the employee ID does not exist, handle the exception and log an error message.
+---Scenario 2: Manage errors when updating employee salaries.
+---o	Question: Write a stored procedure UpdateSalary that increases the salary of an employee by a given percentage. If the employee ID does not exist, handle the exception and log an error message.
 
 
 
