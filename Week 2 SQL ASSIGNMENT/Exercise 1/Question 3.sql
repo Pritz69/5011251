6a2295eb189330e8cf5bc1cf754d133a48f30853
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-08-02 18:56:51 +0530

    Update Question 3.sql

--- Week 2 SQL ASSIGNMENT/Exercise 1/Question 3.sql
+++ Week 2 SQL ASSIGNMENT/Exercise 1/Question 3.sql
@@ -1,5 +1,5 @@
-Scenario 3: The bank wants to send reminders to customers whose loans are due within the next 30 days.
-o	Question: Write a PL/SQL block that fetches all loans due in the next 30 days and prints a reminder message for each customer.
+---Scenario 3: The bank wants to send reminders to customers whose loans are due within the next 30 days.
+---o	Question: Write a PL/SQL block that fetches all loans due in the next 30 days and prints a reminder message for each customer.
 
 
 SET SERVEROUTPUT ON;
