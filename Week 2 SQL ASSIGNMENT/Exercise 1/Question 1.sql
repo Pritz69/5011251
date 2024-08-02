7937b7b1378057e0fbdb5c21636f2898ed24cb0d
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-08-02 18:56:23 +0530

    Update Question 1.sql

--- Week 2 SQL ASSIGNMENT/Exercise 1/Question 1.sql
+++ Week 2 SQL ASSIGNMENT/Exercise 1/Question 1.sql
@@ -1,5 +1,5 @@
-Scenario 1: The bank wants to apply a discount to loan interest rates for customers above 60 years old.
-o	Question: Write a PL/SQL block that loops through all customers, checks their age, and if they are above 60, apply a 1% discount to their current loan interest rates.
+---Scenario 1: The bank wants to apply a discount to loan interest rates for customers above 60 years old.
+---o	Question: Write a PL/SQL block that loops through all customers, checks their age, and if they are above 60, apply a 1% discount to their current loan interest rates.
 
 
 UPDATE Loans
