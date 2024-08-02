736d79c0fb2bd781e545425a1b6aabb531a842e4
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-08-02 18:57:07 +0530

    Update Question 1.sql

--- Week 2 SQL ASSIGNMENT/Exercise 2/Question 1.sql
+++ Week 2 SQL ASSIGNMENT/Exercise 2/Question 1.sql
@@ -1,5 +1,5 @@
-Scenario 1: Handle exceptions during fund transfers between accounts.
-o	Question: Write a stored procedure SafeTransferFunds that transfers funds between two accounts. Ensure that if any error occurs (e.g., insufficient funds), an appropriate error message is logged and the transaction is rolled back.
+---Scenario 1: Handle exceptions during fund transfers between accounts.
+---o	Question: Write a stored procedure SafeTransferFunds that transfers funds between two accounts. Ensure that if any error occurs (e.g., insufficient funds), an appropriate error message is logged and the transaction is rolled back.
 
 
 SET SERVEROUTPUT ON;
