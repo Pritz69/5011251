05b114589ea914eae84113faf8894884b33cc4e3
Prithwish Sarkar <98264659+Pritz69@users.noreply.github.com>
2024-08-02 18:58:04 +0530

    Update Question 2.sql

--- Week 2 SQL ASSIGNMENT/Exercise 3/Question 2.sql
+++ Week 2 SQL ASSIGNMENT/Exercise 3/Question 2.sql
@@ -1,5 +1,5 @@
-Scenario 2: The bank wants to implement a bonus scheme for employees based on their performance.
-o	Question: Write a stored procedure UpdateEmployeeBonus that updates the salary of employees in a given department by adding a bonus percentage passed as a parameter.
+---Scenario 2: The bank wants to implement a bonus scheme for employees based on their performance.
+---o	Question: Write a stored procedure UpdateEmployeeBonus that updates the salary of employees in a given department by adding a bonus percentage passed as a parameter.
 
 
 CREATE OR REPLACE PROCEDURE UpdateEmployeeBonus (
